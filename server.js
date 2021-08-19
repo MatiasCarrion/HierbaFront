3b1a5559af275272a92e1fc86e9453c324b928e0
MatiasCarrion <matiasezequielcarrion@gmail.com>
2021-08-19 19:42:25 -0300

    probando otra confi en server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/front'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/front/'}),
+// );
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
+const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/front'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/front'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/front/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
